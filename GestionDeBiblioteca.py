17fa1911ab3096bd8a310dee7b7a5cd29b43f44b
quirozcitlali343-art <quirozcitlali343@gmail.com>
2026-06-20 21:20:03 -0600

    agrego

---
+++ GestionDeBiblioteca.py
@@ -0,0 +1,10 @@
+# Reto 3 - Gestion de Biblioteca
+# Taller de Python Intermedio
+ 
+# aqui guardo todos los libros de la biblioteca
+libros = []
+ 
+# aqui guardo los usuarios y los libros que tienen prestados
+usuarios = {}
+ 
+ 
