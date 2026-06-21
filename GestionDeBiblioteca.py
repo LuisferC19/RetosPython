96a47ea449633eb25bda3f9a66b96262678db8ae
Luis Fernando R.S <fernandocort7@gmail.com>
2026-06-20 23:32:42 -0600

    Se agrego la funcion de para devolver un libro

--- GestionDeBiblioteca.py
+++ GestionDeBiblioteca.py
@@ -8,3 +8,26 @@ libros = []
 usuarios = {}
  
  
+ # funcion para devolver un libro
+def devolver_libro(nombre_usuario, titulo):
+    if nombre_usuario not in usuarios:
+        print(f"{nombre_usuario} no esta registrado")
+        return
+ 
+    if titulo not in usuarios[nombre_usuario]:
+        print(f"{nombre_usuario} no tiene prestado ese libro")
+        return
+ 
+    for l in libros:
+        if l["titulo"].lower() == titulo.lower():
+            l["copias"] += 1
+            usuarios[nombre_usuario].remove(titulo)
+            print(f"{nombre_usuario} devolvio '{titulo}', gracias!")
+            return
+ 
+ 
+# generador para obtener solo los libros que tienen copias
+def libros_disponibles():
+    for l in libros:
+        if l["copias"] > 0:
+            yield l
